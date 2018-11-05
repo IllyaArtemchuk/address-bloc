8926f1002dd461779d3ea094a06c8804ac060ce8
Illya Artemchuk <ilyalinkis@gmail.com>
2018-11-04 19:00:20 -0800

    Checkpoint 3 complete

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -4,7 +4,6 @@ const sequelize = require("../db/models/index").sequelize;
 describe("ContactController", ()=> {
     beforeEach((done)=> {
         this.book = new ContactController();
-
         sequelize.sync({force: true}).then((res) => {
             done();
           })
@@ -16,10 +15,16 @@ describe("ContactController", ()=> {
         expect(ContactController).toBeDefined();
     })
     describe("#addContact()", ()=> {
-        it("should add a single contact into the book", ()=> {
-            expect(this.book.contacts.length).toBe(0);
-            this.book.addContact("Alice", "001-101-1010");
-            expect(this.book.contacts.length).toBe(1);
+        it("should add a single contact into the book", (done)=> {
+            this.book.addContact("Alice", "001-101-1010")
+            .then((contact)=> {
+                expect(contact.name).toBe("Alice");
+                expect(contact.phone).toBe("001-101-1010");
+                done();
+            })
+            .catch((err)=> {
+                done();
+            });
         });
     });
 })
