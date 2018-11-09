5afc388fa81e50de6bcbed000ce146a8a0e2326d
Illya Artemchuk <ilyalinkis@gmail.com>
2018-11-08 22:34:18 -0800

    Completed Checkpoint

--- Controllers/MenuController.js
+++ Controllers/MenuController.js
@@ -11,9 +11,10 @@ module.exports = class MenuController {
                 message: "Please choose from an option below",
                 choices: [
                     "Add new contact",
-                    "Exit",
                     "View all contacts",
-                    "Get Date"
+                    "Get Date",
+                    "Search for a contact",
+                    "Exit"
                 ]
             }
         ];
@@ -37,7 +38,10 @@ module.exports = class MenuController {
                     break;
                 case "View all contacts":
                     this.getContacts();
-                    break
+                    break;
+                case "Search for a contact":
+                    this.search();
+                    break;
                 default:
                     console.log("Invalid Input");
                     this.main();
@@ -69,6 +73,68 @@ module.exports = class MenuController {
         })
     }
 
+    search() {
+        inquirer.prompt(this.book.searchQuestions).then((target)=> {
+            this.book.search(target.name).then((contact)=> {
+                if(contact === null) {
+                    this.clear();
+                    console.log("contact not found");
+                    this.search(); } else {
+                        this.showContact(contact);
+                    }
+            });
+        }).catch((err)=> {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    showContact(contact){
+        this._printContact(contact);
+        inquirer.prompt(this.book.showContactQuestions).then((answer)=> {
+            switch(answer.selected){
+                case "Delete Contact": 
+                    this.delete(contact)
+                    break;
+                case "Main Menu":
+                    this.main();
+                    break;
+                default:
+                    console.log("something went wrong")
+                    this.showContact(contact);
+            }
+        }).catch((err)=> {
+            console.log(err);
+            this.showContact(contact)
+        })
+    }
+
+    delete(contact){
+        inquirer.prompt(this.book.deleteConfirmQuestions).then((answer)=> {
+            if(answer.confirmation){
+                this.book.delete(contact.id)
+                console.log("Contact Deleted!")
+                this.main();
+            } else {
+                console.log("contact not deletec");
+                this.showContact(contact);
+            }
+        }).catch((err)=> {
+            console.log(err);
+            this.main();
+        });
+    }
+
+    _printContact(contact){
+        console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+        );
+
+    }
+
     getDate() {
         this.clear();
         console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
@@ -84,12 +150,7 @@ module.exports = class MenuController {
 
         this.book.getContacts().then((contacts)=> {
             for(let contact of contacts) {
-                console.log(`
-                name: ${contact.name}
-                phone number: ${contact.phone}
-                email: ${contact.email}
-                ---------------`
-                );
+                this._printContact(contact);
             }
             this.main();
         }).catch((err)=> {
